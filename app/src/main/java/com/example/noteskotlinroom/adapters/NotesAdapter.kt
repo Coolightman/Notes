854db3e576aa7b80d7d6b1b46177348ebc367050
Coolightman <drugak48@gmail.com>
2021-10-31 21:39:01 +0300

    fast fix

--- app/src/main/java/com/example/noteskotlinroom/adapters/NotesAdapter.kt
+++ app/src/main/java/com/example/noteskotlinroom/adapters/NotesAdapter.kt
@@ -6,11 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteskotlinroom.R
 import com.example.noteskotlinroom.entities.Note
-import com.example.noteskotlinroom.enums.DayOfWeek
 
 class NotesAdapter(
     private var notes: List<Note>,
