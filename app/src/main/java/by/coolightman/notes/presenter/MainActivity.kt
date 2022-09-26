5d7b2288022a1f2c5b77e1f7891f7291fdc8a035
coolightman <drugak48@gmail.com>
2022-09-26 10:42:54 +0300

    prepareUI

--- app/src/main/java/by/coolightman/notes/presenter/MainActivity.kt
+++ app/src/main/java/by/coolightman/notes/presenter/MainActivity.kt
@@ -3,10 +3,7 @@ package by.coolightman.notes.presenter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
+import by.coolightman.notes.ui.compose.PrepareUI
 import by.coolightman.notes.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,10 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotesTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
+                PrepareUI {
 
                 }
             }
---
+++ app/src/main/java/by/coolightman/notes/ui/compose/PrepareUI.kt
@@ -0,0 +1,35 @@
+package by.coolightman.notes.ui.compose
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@Composable
+fun PrepareUI(content: @Composable () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        DefineSystemBarsColor()
+        content()
+    }
+}
+
+@Composable
+private fun DefineSystemBarsColor() {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = !isSystemInDarkTheme()
+    val statusBarColor = MaterialTheme.colors.background
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = statusBarColor,
+            darkIcons = useDarkIcons
+        )
+    }
+}
