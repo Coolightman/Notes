55e6b597709409299cda391d0591e35d86273290
coolightman <drugak48@gmail.com>
2022-09-28 15:26:52 +0300

    add navigation animation

--- app/src/main/java/by/coolightman/notes/presenter/MainActivity.kt
+++ app/src/main/java/by/coolightman/notes/presenter/MainActivity.kt
@@ -3,26 +3,28 @@ package by.coolightman.notes.presenter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
 import by.coolightman.notes.ui.compose.AppBottomBar
 import by.coolightman.notes.ui.compose.PrepareUI
 import by.coolightman.notes.ui.model.StartDestination
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PrepareUI {
-                val navController = rememberNavController()
+                val navController = rememberAnimatedNavController()
                 val startDestination = StartDestination.NOTES.route
                 Scaffold(
                     scaffoldState = rememberScaffoldState(),
