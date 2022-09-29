bc9f7578f080a58cd8213096a4d135fa55cb3dd9
coolightman <drugak48@gmail.com>
2022-09-29 20:04:57 +0300

    fix splash blink

--- app/src/main/java/by/coolightman/notes/ui/screens/notesScreen/NotesScreen.kt
+++ app/src/main/java/by/coolightman/notes/ui/screens/notesScreen/NotesScreen.kt
@@ -2,12 +2,13 @@ package by.coolightman.notes.ui.screens.notesScreen
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import by.coolightman.notes.R
 import by.coolightman.notes.ui.components.EmptyContentSplash
 import by.coolightman.notes.ui.components.NotesItem
+import kotlinx.coroutines.delay
 
 @Composable
 fun NotesScreen(
@@ -16,11 +17,21 @@ fun NotesScreen(
 ) {
     val state = viewModel.uiState
 
+    var isSplashVisible by remember {
+        mutableStateOf(false)
+    }
+
     if (state.list.isEmpty()) {
-        EmptyContentSplash(
-            iconId = R.drawable.ic_outline_note_64,
-            textId = R.string.no_notes
-        )
+        LaunchedEffect(Unit){
+            delay(200)
+            isSplashVisible = true
+        }
+        if (isSplashVisible) {
+            EmptyContentSplash(
+                iconId = R.drawable.ic_outline_note_64,
+                textId = R.string.no_notes
+            )
+        }
     } else {
         LazyColumn {
             items(
