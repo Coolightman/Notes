0ab1ceb0a876b006508770812d901553470df64d
coolightman <drugak48@gmail.com>
2022-10-13 15:16:45 +0300

    update animations

--- app/src/main/java/by/coolightman/notes/ui/navigation/AppNavigationHost.kt
+++ app/src/main/java/by/coolightman/notes/ui/navigation/AppNavigationHost.kt
@@ -1,9 +1,13 @@
 package by.coolightman.notes.ui.navigation
 
 import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.material.ScaffoldState
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
@@ -25,7 +29,8 @@ import by.coolightman.notes.util.ARG_TASK_ID
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 
-private const val TRANSITION_DURATION = 300
+private const val TRANSITION_DURATION = 100
+private const val SLIDE_DURATION = 300
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -39,24 +44,21 @@ fun AppNavigationHost(
     val density = LocalDensity.current
     val displayWidth by remember {
         mutableStateOf(
-            with(density){configuration.screenWidthDp.dp.toPx().toInt()}
-        )
-    }
-    val displayHeight by remember {
-        mutableStateOf(
-            with(density){configuration.screenHeightDp.dp.toPx().toInt()}
+            with(density) { configuration.screenWidthDp.dp.toPx().toInt() }
         )
     }
 
     AnimatedNavHost(
         navController = navController,
         startDestination = startDestination,
+        enterTransition = { fadeIn(animationSpec = tween(TRANSITION_DURATION)) },
+        exitTransition = { fadeOut(animationSpec = tween(TRANSITION_DURATION)) }
     ) {
 
         composable(
             route = NavRoutes.Splash.route,
-            enterTransition = { fadeIn(animationSpec = tween(10)) },
-            exitTransition = { fadeOut(animationSpec = tween(10)) },
+            enterTransition = { fadeIn(animationSpec = tween(TRANSITION_DURATION)) },
+            exitTransition = { fadeOut(animationSpec = tween(TRANSITION_DURATION)) }
         ) {
             SplashScreen()
         }
@@ -66,7 +68,10 @@ fun AppNavigationHost(
             enterTransition = {
                 when (initialState.destination.route) {
                     NavRoutes.Tasks.route -> {
-                        slideInHorizontally(initialOffsetX = { -displayWidth })
+                        slideInHorizontally(
+                            initialOffsetX = { -displayWidth },
+                            animationSpec = tween(SLIDE_DURATION, easing = FastOutSlowInEasing)
+                        ) + fadeIn(animationSpec = tween(200))
                     }
                     else -> fadeIn(animationSpec = tween(TRANSITION_DURATION))
                 }
@@ -74,7 +79,10 @@ fun AppNavigationHost(
             exitTransition = {
                 when (targetState.destination.route) {
                     NavRoutes.Tasks.route -> {
-                        slideOutHorizontally(targetOffsetX = { -displayWidth })
+                        slideOutHorizontally(
+                            targetOffsetX = { -displayWidth },
+                            animationSpec = tween(SLIDE_DURATION, easing = FastOutSlowInEasing)
+                        ) + fadeOut(animationSpec = tween(200))
                     }
                     else -> fadeOut(animationSpec = tween(TRANSITION_DURATION))
                 }
@@ -89,13 +97,13 @@ fun AppNavigationHost(
 
         composable(
             route = NavRoutes.EditNote.route + "/{$ARG_NOTE_ID}",
-            enterTransition = { fadeIn(animationSpec = tween(TRANSITION_DURATION)) },
-            exitTransition = { fadeOut(animationSpec = tween(TRANSITION_DURATION)) },
             arguments = listOf(
                 navArgument(ARG_NOTE_ID) {
                     type = NavType.LongType
                 }
-            )
+            ),
+            enterTransition = { fadeIn(animationSpec = tween(TRANSITION_DURATION)) },
+            exitTransition = { fadeOut(animationSpec = tween(TRANSITION_DURATION)) }
         ) {
             EditNoteScreen(
                 navController = navController,
@@ -128,7 +136,10 @@ fun AppNavigationHost(
             enterTransition = {
                 when (initialState.destination.route) {
                     NavRoutes.Notes.route -> {
-                        slideInHorizontally(initialOffsetX = { displayWidth })
+                        slideInHorizontally(
+                            initialOffsetX = { displayWidth },
+                            animationSpec = tween(SLIDE_DURATION, easing = FastOutSlowInEasing)
+                        ) + fadeIn(animationSpec = tween(200))
                     }
                     else -> fadeIn(animationSpec = tween(TRANSITION_DURATION))
                 }
@@ -136,7 +147,10 @@ fun AppNavigationHost(
             exitTransition = {
                 when (targetState.destination.route) {
                     NavRoutes.Notes.route -> {
-                        slideOutHorizontally(targetOffsetX = { displayWidth })
+                        slideOutHorizontally(
+                            targetOffsetX = { displayWidth },
+                            animationSpec = tween(SLIDE_DURATION, easing = FastOutSlowInEasing)
+                        ) + fadeOut(animationSpec = tween(200))
                     }
                     else -> fadeOut(animationSpec = tween(TRANSITION_DURATION))
                 }
@@ -150,13 +164,13 @@ fun AppNavigationHost(
 
         composable(
             route = NavRoutes.EditTask.route + "/{$ARG_TASK_ID}",
-            enterTransition = { fadeIn(animationSpec = tween(TRANSITION_DURATION)) },
-            exitTransition = { fadeOut(animationSpec = tween(TRANSITION_DURATION)) },
             arguments = listOf(
                 navArgument(ARG_TASK_ID) {
                     type = NavType.LongType
                 }
-            )
+            ),
+            enterTransition = { fadeIn(animationSpec = tween(TRANSITION_DURATION)) },
+            exitTransition = { fadeOut(animationSpec = tween(TRANSITION_DURATION)) }
         ) {
             EditTaskScreen(
                 navController = navController,
