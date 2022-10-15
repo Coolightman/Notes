c900d539cf76c076268bd0955bfdb5e6bd07d8cf
coolightman <drugak48@gmail.com>
2022-10-15 15:34:00 +0300

    remove notification when inactive task

--- app/src/main/java/by/coolightman/notes/domain/usecase/tasks/SwitchTaskActivityUseCase.kt
+++ app/src/main/java/by/coolightman/notes/domain/usecase/tasks/SwitchTaskActivityUseCase.kt
@@ -1,16 +1,28 @@
 package by.coolightman.notes.domain.usecase.tasks
 
+import by.coolightman.notes.domain.model.Task
 import by.coolightman.notes.domain.repository.TaskRepository
+import java.util.*
 import javax.inject.Inject
 
 class SwitchTaskActivityUseCase @Inject constructor(
-    private val repository: TaskRepository
+    private val repository: TaskRepository,
+    private val removeNotificationUseCase: RemoveNotificationUseCase
 ) {
-    suspend operator fun invoke(taskId: Long){
+    suspend operator fun invoke(taskId: Long) {
         val task = repository.getTask(taskId)
         val switched = task.copy(
-            isActive = !task.isActive
+            isActive = !task.isActive,
+            isHasNotification = false,
+            notificationTime = Calendar.getInstance()
         )
+        updateNotification(task)
         repository.update(switched)
+    }
+
+    private fun updateNotification(task: Task) {
+        if (!task.isActive) {
+            removeNotificationUseCase(task.id.toInt())
+        }
     }
 }
