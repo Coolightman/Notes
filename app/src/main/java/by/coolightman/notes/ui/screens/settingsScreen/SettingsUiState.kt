f9fc2d77a38dba72e86c78a2d07168acd3ea139c
coolightman <drugak48@gmail.com>
2022-10-18 11:16:12 +0300

    fix

--- app/src/main/java/by/coolightman/notes/ui/screens/settingsScreen/SettingsUiState.kt
+++ app/src/main/java/by/coolightman/notes/ui/screens/settingsScreen/SettingsUiState.kt
@@ -1,10 +1,11 @@
 package by.coolightman.notes.ui.screens.settingsScreen
 
+import by.coolightman.notes.ui.model.NavRoutes
 import by.coolightman.notes.ui.model.NotesViewMode
 import by.coolightman.notes.ui.model.ThemeMode
 
 data class SettingsUiState(
-    val appStartDestination: String = "",
+    val appStartDestination: String = NavRoutes.Notes.route,
     val themeMode: ThemeMode = ThemeMode.SYSTEM_MODE,
     val newNoteColorIndex: Int = 0,
     val newTaskColorIndex: Int = 0,
