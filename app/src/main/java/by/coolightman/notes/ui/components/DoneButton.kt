72e04a19399cb581013c9422a58745fd0552d394
coolightman <drugak48@gmail.com>
2022-10-19 11:14:28 +0300

    update done bt

--- app/src/main/java/by/coolightman/notes/ui/components/DoneButton.kt
+++ app/src/main/java/by/coolightman/notes/ui/components/DoneButton.kt
@@ -1,45 +1,32 @@
 package by.coolightman.notes.ui.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DoneButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
-    Surface(
-        onClick = onClick,
-        modifier = modifier.padding(16.dp),
-        shape = MaterialTheme.shapes.small.copy(CornerSize(percent = 50)),
-        color = MaterialTheme.colors.primary,
-        contentColor = MaterialTheme.colors.onSurface,
-        elevation = 6.dp
-    ) {
-        Box(modifier = Modifier
-            .size(56.dp)
-            .clickable { onClick() }
-        ) {
+    Button(
+        modifier = modifier
+            .padding(12.dp)
+            .height(40.dp),
+        shape = RoundedCornerShape(16.dp),
+        onClick = { onClick() },
+        content = {
             Icon(
                 imageVector = Icons.Default.Done,
                 contentDescription = "done",
-                modifier = Modifier.align(Alignment.Center)
+                tint = MaterialTheme.colors.onSurface.copy(LocalContentAlpha.current)
             )
         }
-    }
-
+    )
 }
