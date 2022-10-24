74020f37b541ff837e29c6d08bb1fdb57c3b6033
coolightman <drugak48@gmail.com>
2022-10-24 17:14:15 +0300

    update notification by priority and cancel button

---
+++ app/src/main/java/by/coolightman/notes/broadcastReceiver/CancelNotificationReceiver.kt
@@ -0,0 +1,17 @@
+package by.coolightman.notes.broadcastReceiver
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import by.coolightman.notes.util.NOTIFICATION_ID_EXTRA
+
+class CancelNotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationId = intent.getIntExtra(NOTIFICATION_ID_EXTRA, 0)
+        notificationManager.cancel(notificationId)
+    }
+}
--- app/src/main/java/by/coolightman/notes/broadcastReceiver/NotificationReceiver.kt
+++ app/src/main/java/by/coolightman/notes/broadcastReceiver/NotificationReceiver.kt
@@ -1,11 +1,11 @@
 package by.coolightman.notes.broadcastReceiver
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
@@ -22,35 +22,51 @@ class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val notificationManager =
-            context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationId = intent.getIntExtra(NOTIFICATION_ID_EXTRA, 0)
+        val notificationTitle =
+            context.resources.getString(R.string.task_notification_title) + " " + context.getString(
+                R.string.task_for_time
+            ) + " " + intent.getStringExtra(NOTIFICATION_TIME)
+        val notificationText = intent.getStringExtra(NOTIFICATION_TEXT_EXTRA)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
                 NOTIFICATION_CHANNEL_ID,
                 NOTIFICATION_CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationChannel.description = NOTIFICATION_CHANNEL_DESCRIPTION
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = NOTIFICATION_CHANNEL_DESCRIPTION
+            }
             notificationManager.createNotificationChannel(notificationChannel)
         }
 
-        val launchAppIntent = Intent(context, MainActivity::class.java)
+        val launchAppIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
         val launchAppPendingIntent =
             PendingIntent.getActivity(context, 0, launchAppIntent, PendingIntent.FLAG_IMMUTABLE)
 
+        val okBtIntent = Intent(context, CancelNotificationReceiver::class.java).apply {
+            putExtra(NOTIFICATION_ID_EXTRA, notificationId)
+        }
+        val okBtPendingIntent = PendingIntent.getBroadcast(context, 0, okBtIntent, PendingIntent.FLAG_IMMUTABLE)
+
         val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-            .setContentTitle(
-                context.resources.getString(R.string.task_notification_title) + " " +
-                    context.getString(R.string.task_for_time) + " " +
-                    intent.getStringExtra(NOTIFICATION_TIME)
-            )
-            .setContentText(intent.getStringExtra(NOTIFICATION_TEXT_EXTRA))
             .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(notificationTitle)
+            .setContentText(notificationText)
+            .setStyle(NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(Notification.DEFAULT_ALL)
             .setContentIntent(launchAppPendingIntent)
+            .addAction(
+                R.drawable.ic_round_done_24,
+                context.resources.getString(R.string.okay),
+                okBtPendingIntent
+            )
             .setAutoCancel(true)
             .build()
-
-        val notificationId = intent.getIntExtra(NOTIFICATION_ID_EXTRA, 0)
 
         notificationManager.notify(notificationId, notification)
     }
